d0c7b1d507703318a6f517eaee9dc4e48d0a1c28
Sidhardh Joe <sidhardhjoe@email.com>
2024-05-17 13:04:23 +0530

    Fonts

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Design from './Components/Design';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      
+      <Design></Design>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#e3e7e7',
+    flex: 1,s
   },
 });
