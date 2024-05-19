9ffd44e304c4503a040fa6b369e64461459f46cc
Sidhardh Joe <sidhardhjoe@email.com>
2024-05-19 11:17:52 +0530

    CardPage

--- Components/CardPage.js
+++ Components/CardPage.js
@@ -1,24 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
-import LinearGradient from 'react-native-linear-gradient';
 
 
 const CardPage = () => {
   return (
-    <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.carddesign}>
-    </LinearGradient>
+    <View style={styles.carddesign}>
+      <View style={{ flexDirection: "row" }}>
+        <View >
+          <Text style={styles.text1}>
+            0125 0125 0125 0125
+          </Text>
+          <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+            <Text style={styles.text2}>
+              SIDHARDH JOE
+            </Text>
+            <Text style={styles.text3}>
+              22/22
+            </Text>
+          </View>
+        </View>
+
+        <Image source={require("../Icons/MasterCard.png")} style={styles.cardlogo} />
+      </View>
+    </View>
   )
 }
 
 export default CardPage
 
 const styles = StyleSheet.create({
-    carddesign:{
-        height:220,
-        width:360,
-        backgroundColor:"purple-red",
-        borderRadius:20,
-        marginLeft:15,
-        marginRight:10
-    }
+  carddesign: {
+    height: 220,
+    width: 360,
+    backgroundColor: "black",
+    borderRadius: 20,
+    marginLeft: 15,
+    marginRight: 10,
+    backgroundColor: 'rgba(0,0,0,0.4)'
+  },
+  text1: {
+    color: "black",
+    fontFamily: "PoppinsRegular",
+    fontSize: 25,
+    marginTop: "50%",
+    marginLeft: "6%"
+  },
+  text2: {
+    color: "black",
+    fontFamily: "PoppinsBold",
+    fontSize: 18,
+    marginLeft: "6%"
+  },
+  text3:{
+    color: "black",
+    fontFamily: "PoppinsRegular",
+    fontSize: 18,
+  },
+  cardlogo: {
+    height: 50,
+    width: 60,
+    marginTop: "2%",
+    marginLeft: "7%"
+  }
 })
